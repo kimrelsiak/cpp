631e487a011f9f9f0820dce3442591eb9287daa5
kimrelsiak <96698254+kimrelsiak@users.noreply.github.com>
2022-05-06 07:58:18 +0200

    poprawiony jas

--- Ep1/ImieninyJasia/imieninyRozwiazanie.cpp
+++ Ep1/ImieninyJasia/imieninyRozwiazanie.cpp
@@ -6,17 +6,17 @@ int kaszojady, cukierki, x, y;
 
 int main()
 {
-    cout<<"Ilu kaszojadow jest w Twojej klasie: "endl;
+    cout<<"Ilu kaszojadow jest w Twojej klasie: ";
     cin>>kaszojady;
 
-    cout<<"Ile cukierkow kupiles: "endl;
+    cout<<"Ile cukierkow kupiles: ";
     cin>>cukierki;
 
     x=cukierki/(kaszojady-1);
-    cout<<"Cukierkow dla kazdego kaszojada: "<<x endl;
+    cout<<"Cukierkow dla kazdego kaszojada: "<<x;
 
     y=cukierki-x*(kaszojady-1);
-    cout<<endl<<"Jasiek bedzie mial jeszcze: "<<y endl; 
+    cout<<endl<<"Jasiek bedzie mial jeszcze: "<<y; 
 
     return 0;
 }
