1d8e27925b2a6d5d1930c7a1427efbdbb1116265
Jarek <jar.ek@vp.pl>
2022-05-07 15:41:23 +0200

    update
    Alt code for onlinegdb

--- Ep3/PetlaForRakieta/PetlaForRakieta.cpp
+++ Ep3/PetlaForRakieta/PetlaForRakieta.cpp
@@ -16,3 +16,30 @@ int main()
 
     return 0
 }
+
+/*
+
+Wersja dla kompilatora linuxowego:
+
+#include <iostream>
+#include <unistd.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    
+
+    for (int i=15; i>=0; i--)
+    {   
+        sleep(1);
+        system("clear");
+        cout<<i<<endl;
+    }
+    cout << "Jebut";
+
+    return 0;
+}
+
+*/
