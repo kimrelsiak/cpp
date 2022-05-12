66bae5e905b3fb7781ecacabd32f4d9c166c158b
kimrelsiak <96698254+kimrelsiak@users.noreply.github.com>
2022-05-12 08:18:23 +0200

    Update euklidesmodulo.cpp

---
+++ AlgorytmEuklidesa/euklidesmodulo.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int a,b;
+
+int main()
+{
+    cout<<"Podaj liczbe a:"<<endl;
+    cin>>a;
+    cout<<"Podaj liczbe b:"<<endl;
+    cin>>b;
+
+    cout<<a<<endl;
+    cout<<b<<endl;
+
+    cout<<"Największy wspólny dzielnik to:"<<a<<endl;
+
+
+    return 0;
+}
